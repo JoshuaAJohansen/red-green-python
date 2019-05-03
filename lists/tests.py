05390e3a007bddc1f7da02af03bbf73ebf17341f
Josh Johansen <josh@johansen.us.com>
2019-05-03 08:57:25 -0700

    Work in progress: posting variables to template.

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
-
+from django.http import HttpResponse
+from django.shortcuts import  render
 from lists.views import home_page
 
 
@@ -12,4 +13,9 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
