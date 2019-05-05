6a05285472bce714207aea473439907d7a702b23
Josh Johansen <josh@johansen.us.com>
2019-05-04 20:39:21 -0700

    Added helper function to dynamically wait for 'body' to load

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,14 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def enter_and_wait_for_body_to_load(self, element):
+        body = self.browser.find_element_by_tag_name('body')
+        newBody = body
+        element.send_keys(Keys.ENTER)
+        while body == newBody:
+            time.sleep(0.05)
+            newBody = self.browser.find_element_by_tag_name('body')
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -43,15 +51,13 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.ENTER)
-        # time.sleep(1)
+        self.enter_and_wait_for_body_to_load(inputbox)
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-        # time.sleep(1)
+        self.enter_and_wait_for_body_to_load(inputbox)
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
