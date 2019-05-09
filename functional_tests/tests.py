35a968a351e680bd325788d662540ec6b7aa602a
Josh Johansen <josh@johansen.us.com>
2019-05-08 18:22:50 -0700

    moved functional test into package and added django test runner LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -29,7 +28,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         # assert 'To-Do' in browser.title, "Browser title was " + browser.title
@@ -73,6 +72,3 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfied, she goes back to sleep
         self.fail('Finish the test!')
 
-
-if __name__ == '__main__':
-    unittest.main()
